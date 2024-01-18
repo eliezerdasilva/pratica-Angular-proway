622d93eedcf384a583be58b1bf98ddff3e933dee
Eliézer da Silva <eliezerdasilva2005@gmail.com>
2024-01-17 21:32:53 -0300

    :wrench: improvement: package adjustments

---
+++ Roteamento/src/app/segunda-pagina/segunda-pagina.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-pagina',
+  templateUrl: './segunda-pagina.component.html',
+  styleUrl: './segunda-pagina.component.css'
+})
+export class SegundaPaginaComponent {
+
+}
